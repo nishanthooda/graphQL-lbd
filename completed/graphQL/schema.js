091cf0c63af5de5708ca3621bf9a7581336f0273
Matthew Yuen <un.matthew@gmail.com>
2019-04-08 23:40:46 -0400

    Add descriptions to schemas and fix formatting in index.js

--- completed/graphQL/schema.js
+++ completed/graphQL/schema.js
@@ -1,11 +1,13 @@
 const { buildSchema } = require("graphql");
 
 module.exports = buildSchema(`
+    "Includes all the information about employee salaries."
     type Salary {
         amount: Float!
         currency: String!
     }
 
+    "Includes all the information about employees."
     type Employee {
         name: String!
         email: String!
@@ -14,6 +16,7 @@ module.exports = buildSchema(`
         salary: Salary!
     }
 
+    "Information required to create an employee."
     input EmployeeData {
         name: String!
         email: String!
@@ -21,13 +24,18 @@ module.exports = buildSchema(`
         height: Float!
     }
 
+    "All the defined queries."
     type RootQuery {
+        "Returns a list of all employees."
         employees: [Employee!]
+        "Returns a single employee."
         employee(email: String!): Employee
     }
 
     type RootMutation {
+        "Creates an employee."
         createEmployee(employeeInput: EmployeeData): Employee!
+        "Deletes an employee."
         deleteEmployee(email: String!) : Employee
     }
 
--- completed/index.js
+++ completed/index.js
@@ -1,19 +1,21 @@
-const express = require('express')
-const graphqlHttp = require('express-graphql')
-const graphqlSchema = require('./graphQL/schema')
-const graphqlResolver = require('./graphQL/resolver')
+const express = require("express");
+const graphqlHttp = require("express-graphql");
+const graphqlSchema = require("./graphQL/schema");
+const graphqlResolver = require("./graphQL/resolver");
 
+const app = express();
 
-const app = express()
+app.get("/", (req, res) => {
+  res.send("here");
+});
 
-app.get('/', (req, res) => {
-    res.send("here")
-})
-
-app.use('/graphql', graphqlHttp({
+app.use(
+  "/graphql",
+  graphqlHttp({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     graphiql: true
-}))
+  })
+);
 
-app.listen(8080, 'localhost')
+app.listen(8080, "localhost");
